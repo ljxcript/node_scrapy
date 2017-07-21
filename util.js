563a4b38ff721073e5162344d6371b7ff2fb1d5a
廖竞翔 <Liaojx3@mail2.sysu.edu.cn>
2017-07-21 18:48:45 +0800

    Update util.js

--- util.js
+++ util.js
@@ -4,9 +4,17 @@ function isDetailLink(link) {
 	return link.indeOf('book.douban.com/subject/') !== -1;
 }
 
+function escapeSpace(s) {
+	return escapeQuote(s.split(/\s+/).join(''));
+}
+
+function escapeQuote(s){
+	return s.replace('"', ' ').replace("'", ' ');
+}
 
 var util = {
-	isDetailLink: isDetailLink
+	isDetailLink: isDetailLink,
+	escapeSpace: escapeSpace,
 }
 
 
